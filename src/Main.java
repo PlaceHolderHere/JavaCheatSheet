ca85ad831c4cf8a3232c66f6f687878bd9871f42
PlaceHolderHere <holderhereinplace@gmail.com>
2026-04-28 13:19:57 +0800

    Added primiteTypes() for basic Java data types

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        primitiveTypes();
+    }
+
+    static void primitiveTypes(){
+        byte a = 2; // 1 byte [-2^8, 2^8 - 1]
+        short b = 2000; // 2 bytes [-2^16, 2^16 - 1]
+        int c = 100000; // 4 bytes [-2^31, 2^31 - 1]
+        long d = 999_999_999; // 8 bytes
+        float price = 9.99F; // 4 bytes
+        double pi = 3.14159; // 8 bytes
+        char letter = 'A';  // one character
+        boolean isEighteen = false;  // true or false
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(price);
+        System.out.println(pi);
+        System.out.println(letter);
+        System.out.println(isEighteen);
     }
 }
