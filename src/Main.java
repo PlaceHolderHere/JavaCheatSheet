a67a7c3611ac1fe74eb2f50d175ee21c1a77e745
PlaceHolderHere <holderhereinplace@gmail.com>
2026-04-28 14:44:19 +0800

    Added an example of creating and using a reference type

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-        primitiveTypes();
+//        primitiveTypes();
+        referenceTypes();
     }
 
     static void primitiveTypes(){
@@ -21,5 +24,18 @@ public class Main {
         System.out.println(pi);
         System.out.println(letter);
         System.out.println(isEighteen);
+    }
+
+    static void referenceTypes(){
+        // Strings are a list of characters
+        String message = "Hello World"; // Initializing a String
+        System.out.println(message);
+
+        // Initializing any other Reference Type
+        // Reference Types "Refer" or "Point" to an object in memory (like a C pointer)
+        Point point1 = new Point(10, 5);
+        System.out.println(point1);
+        System.out.println(point1.x);
+        System.out.println(point1.y);
     }
 }
