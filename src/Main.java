52eb110e7c2834b5df3023bc1707ccfc66215af1
PlaceHolderHere <holderhereinplace@gmail.com>
2026-05-03 15:15:30 +0800

    Added Scanner for reading user input in the terminal to the Java Cheat sheet

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.awt.*;  // referenceTypes() example -> Point
 import java.util.Arrays;  // for arrays() and multiDimensionalArrays()
+import java.util.Scanner;  // for readUserInput()
 
 public class Main {
     public static void main(String[] args) {
@@ -10,7 +11,8 @@ public class Main {
 //        multiDimensionalArrays();
 //        operators();
 //        typeCasting();
-        mathFunctions();
+//        mathFunctions();
+        readUserInput();
     }
 
     static void primitiveTypes(){
@@ -180,5 +182,22 @@ public class Main {
         // Accessing a specific index
         emptyGrid[1][1] = 2;
         System.out.println("Empty Grid: " + Arrays.deepToString(emptyGrid));
+    }
+
+    static void readUserInput(){
+        // Using the Scanner object, Java can receive command line input from the user
+        Scanner scanner = new Scanner(System.in); // Reads user input
+
+        // Getting a Byte
+        // Each primitive data type has their own unique next...() function
+        System.out.print("Age: "); // Printing a prompt/question to the user
+        byte age = scanner.nextByte();
+        System.out.println("Age: " + age);
+        scanner.nextLine();
+
+        // Getting a String
+        System.out.print("Name: ");
+        String name = scanner.nextLine();
+        System.out.println("Hello " + name);
     }
 }
