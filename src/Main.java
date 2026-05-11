bc864cb2a355c30b36fe87a07c3fd7be525f152b
PlaceHolderHere <holderhereinplace@gmail.com>
2026-05-11 23:18:24 +0800

    Added switchCases to the Java Cheat sheet

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,8 @@ public class Main {
 //        mathFunctions();
 //        readUserInput();
 //        ifStatements();
-        ternaryOperators();
+//        ternaryOperators();
+        switchCases();
     }
 
     static void primitiveTypes(){
@@ -263,6 +264,21 @@ public class Main {
             int income = 50_000;
             String incomeStatus = income > 15_000 ? "High Income": "Low Income";
             System.out.println(incomeStatus);
-        }
+    }
 
+    static void switchCases(){
+        // A faster way to check if a variable is equal to a certain value
+        String role = "admin";
+        switch (role){
+            case "admin":
+                System.out.println("Hello Admin");
+                break;
+            case "user":
+                System.out.println("Hello User");
+                break;
+            default:
+                System.out.println("Hello Guest");
+                break;
+        }
+    }
 }
